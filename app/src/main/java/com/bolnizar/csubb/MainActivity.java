6b2e7f5f0de65caccd47a6a991525e0d4b0072a6
Boldijar Paul <boldijar.paul@gmail.com>
2016-06-27 21:48:14 +0300

    added bottom bar

--- app/src/main/java/com/bolnizar/csubb/MainActivity.java
+++ app/src/main/java/com/bolnizar/csubb/MainActivity.java
@@ -1,13 +1,49 @@
 package com.bolnizar.csubb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupBottomBar(savedInstanceState);
+    }
+
+    private void setupBottomBar(Bundle savedInstanceState) {
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.setItems(R.menu.bottom_menu);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottom_menu_filter) {
+                    showFilters();
+                } else {
+                    showList();
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+
+            }
+        });
+    }
+
+    private void showList() {
+
+    }
+
+    private void showFilters() {
+
     }
 }
